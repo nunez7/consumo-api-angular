9bc703009e07bfb9e5ebd8eefe2542fa5ce3bcd8
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-02-07 17:31:26 -0700

    Creando el servicio de consulta

---
+++ src/app/services/personajes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonajesService {
+
+  private API_PERSONAJES = "https://rickandmortyapi.com/api/character";
+
+  constructor(private http: HttpClient) { }
+
+  public getAllPersonajes(): Observable<any>{
+    return this.http.get(this.API_PERSONAJES);
+  }
+}

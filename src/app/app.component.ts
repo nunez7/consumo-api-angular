12e119066eaf33f28f54292bd083a9fd140450e9
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-02-07 17:38:10 -0700

    Consumiendo el servicio en component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonajesService } from './services/personajes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Consumo API GET';
+
+  personajes: any = {};
+
+  constructor(private service: PersonajesService){}
+
+  ngOnInit(): void {
+      this.service.getAllPersonajes().subscribe(personajes => {
+        this.personajes = personajes.results;
+        console.log(this.personajes);
+      });
+  }
 }
